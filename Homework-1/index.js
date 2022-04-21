e38a547326e81e7b189073229336832e12164488
sergey.shimin84@gmail.com <sergey.shimin84@gmail.com>
2022-04-21 23:36:53 +0700

    Решение ДЗ к первому уроку

--- Homework-1/index.js
+++ Homework-1/index.js
@@ -2,7 +2,7 @@
 const colors = require("colors");
 
 const isPrime = (number) => {
-  if (number < 2) return false;
+  if (number < 2) return false
 
   for (let i = 2; i <= number / 2; i++) {
     if (number % i === 0) return false;
@@ -17,20 +17,28 @@ const from = process.argv[2];
 const to = process.argv[3];
 
 for (let number = from; number <= to; number++) {
-  console.log(number, isPrime(number))
-  // let colorer = colors.green;
+  let colorer = colors.green;
 
-  // if (isPrime(number)) {
-  //   if (count % 2 === 0) {
-  //     colorer = colors.yellow;
-  //     count += 1;
-  //   } else if (count % 3 === 0) {
-  //     colorer = colors.red;
-  //     count = 1;
-  //   } else {
-  //     count += 1;
-  //   }
+  if (typeof number === 'number') {
+    if (isPrime(number)) {
+      if (count % 2 === 0) {
+        colorer = colors.yellow;
+        count += 1;
+      } else if (count % 3 === 0) {
+        colorer = colors.red;
+        count = 1;
+      } else {
+        count += 1;
+      }
 
-  //   console.log(colorer(number));
-  // }
-}
+      console.log(colorer(number));
+    } else if (number % 2 !== 0) {
+      colorer = colors.red;
+      console.log(colorer('Нет простых чисел.'));
+    }
+
+  } else {
+    colorer = colors.yellow;
+    console.log(colorer('Ошибка, введите число!'));
+  }
+};
