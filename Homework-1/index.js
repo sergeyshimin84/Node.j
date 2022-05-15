57e06f80bd2355a11f06585d5ab4221e0ed77be7
sergey.shimin84@gmail.com <sergey.shimin84@gmail.com>
2022-05-15 23:45:13 +0700

    Homework-1.3

--- Homework-1/index.js
+++ Homework-1/index.js
@@ -1,8 +1,8 @@
 
-const colors = require("colors");
+const colors = require("colors/safe");
 
 const isPrime = (number) => {
-  if (number < 2) return false
+  if (number < 2) return false;
 
   for (let i = 2; i <= number / 2; i++) {
     if (number % i === 0) return false;
@@ -12,33 +12,35 @@ const isPrime = (number) => {
 };
 
 let count = 1;
+let primeCount = 0;
 
-const from = process.argv[2];
-const to = process.argv[3];
+const from = Number(process.argv[2]);
+const to = +process.argv[3];
+
+if (!(isFinite(from) && isFinite(to))) {
+  console.error(colors.red("Входные параметры должны быть числами!!!"));
+  process.exit(1);
+}
 
 for (let number = from; number <= to; number++) {
   let colorer = colors.green;
 
-  if (typeof number === 'number') {
-    if (isPrime(number)) {
-      if (count % 2 === 0) {
-        colorer = colors.yellow;
-        count += 1;
-      } else if (count % 3 === 0) {
-        colorer = colors.red;
-        count = 1;
-      } else {
-        count += 1;
-      }
-
-      console.log(colorer(number));
-    } else if (number % 2 !== 0) {
+  if (isPrime(number)) {
+    primeCount++;
+    if (count % 2 === 0) {
+      colorer = colors.yellow;
+      count += 1;
+    } else if (count % 3 === 0) {
       colorer = colors.red;
-      console.log(colorer('Нет простых чисел.'));
+      count = 1;
+    } else {
+      count += 1;
     }
 
-  } else {
-    colorer = colors.yellow;
-    console.log(colorer('Ошибка, введите число!'));
+    console.log(colorer(number));
   }
-};
+}
+
+if (primeCount === 0) {
+  console.error(colors.red("В последовательности нет простых чисел!!!"));
+}
