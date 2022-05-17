558a5088327710640481442287084d6a298bdcf8
sergey.shimin84@gmail.com <sergey.shimin84@gmail.com>
2022-05-17 21:15:31 +0700

    Подготовлка ДЗ ко второму уроку

--- Homework-1/index.js
+++ Homework-1/index.js
@@ -1,46 +1,19 @@
+console.log("Record 1"); // 1
 
-const colors = require("colors/safe");
+Promise.resolve().then(() => console.log("Record 2")); // 3
 
-const isPrime = (number) => {
-  if (number < 2) return false;
+setTimeout(() => {
+  console.log("Record 3"); // 6
+  Promise.resolve().then(() => {
+    console.log("Record 4"); // 7
+  });
+});
 
-  for (let i = 2; i <= number / 2; i++) {
-    if (number % i === 0) return false;
-  }
+Promise.resolve().then(() => {
+  Promise.resolve().then(() => {
+    console.log("Record 5"); // 5
+  });
+  console.log("Record 6");  // 4
+});
 
-  return true;
-};
-
-let count = 1;
-let primeCount = 0;
-
-const from = +process.argv[2];
-const to = +process.argv[3];
-
-if (!(isFinite(from) && isFinite(to))) {
-  console.error(colors.red("Входные параметры должны быть числами!!!"));
-  process.exit(1);
-}
-
-for (let number = from; number <= to; number++) {
-  let colorer = colors.green;
-
-  if (isPrime(number)) {
-    primeCount++;
-    if (count % 2 === 0) {
-      colorer = colors.yellow;
-      count += 1;
-    } else if (count % 3 === 0) {
-      colorer = colors.red;
-      count = 1;
-    } else {
-      count += 1;
-    }
-
-    console.log(colorer(number));
-  }
-}
-
-if (primeCount === 0) {
-  console.error(colors.red("В последовательности нет простых чисел!!!"));
-}
+console.log("Record 7"); // 2
