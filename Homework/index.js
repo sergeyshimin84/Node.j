ee19a730a2c8949a822820a73dd6e748db4c6a6a
sergey.shimin84@gmail.com <sergey.shimin84@gmail.com>
2022-05-26 22:55:55 +0700

    Подготовка четвертого ДЗ

--- Homework/index.js
+++ Homework/index.js
@@ -1,15 +1,37 @@
-// Подключаем модуль fs. 
+// Подключаем модули.
 const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
 // Подключаем вид потока на чтение и запись Transform модуля stream. 
 const { Transform } = require("stream");
-// Передаем файл с данными.
-const DATA = "./access.log";
+
+const isFile = (fileName) => {
+  return fs.lstatSync(fileName).isFile();
+};
+
+const list = fs.readdirSync(__dirname).filter(isFile);
+
+inquirer
+  .prompt([
+    {
+      name: "fileName",
+      type: "list",
+      message: "Choose file:",
+      choices: list,
+    },
+  ])
+  .then((answer) => {
+    const filePath = path.join(__dirname, answer.fileName);
+    fs.readFile(filePath, "utf8", (err, data) => {
+      console.log(data);
+    });
+  });
 // Передаем массив с ip адресами.
 const IP = ["89.123.1.41", "34.48.240.111"];
 // Создаем файлы с именем данных ip.
 const getOutputFileName = (ip) => `./${ip}_requests.log`;
 // Создаем поток на чтение файла с данными.
-const readStream = fs.createReadStream(DATA, "utf-8");
+const readStream = fs.createReadStream(list, "utf-8");
 // Перебираем массив с помощью метода forEach.
 IP.forEach((ip) => {
   // Проходим по всем совпадениям. С помощью регулярных выражений находим совпадения.
